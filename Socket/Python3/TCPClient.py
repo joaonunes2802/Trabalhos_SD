8da6bbaa258bd3a88b82c243392e838aa5c7d1a6
João Pedro dos Santos Nunes <99223660+joaonunes2802@users.noreply.github.com>
2024-10-07 20:29:49 -0300

    Add files via upload

--- Socket/Python3/TCPClient.py
+++ Socket/Python3/TCPClient.py
@@ -26,17 +26,17 @@ def client(serverName = 'localhost', serverPort = 15000):
     try:
         while True:
             print("Escolha o número da operação desejada:")
-            print("1 - Adição;")
-            print("2 - Subtração;")
-            print("3 - Multiplicação;")
-            print("4 - Divisão;")
+            print("+ - Adição;")
+            print("- - Subtração;")
+            print("* - Multiplicação;")
+            print("/ - Divisão;")
             print("5 - Encerra o programa.")
             operacao = input()
             print("OBS: Escolha no máximo 20 números, separados pela tecla 'espaço'!!")
             if operacao == 1:
                 #Escolha dos numeros
                 numeros = input()
-                dados = numeros + ' 1'  #Adiciona 1 ao final da string de numeros para indicar que é para realizar uma adição
+                dados = numeros + ' +'  #Adiciona 1 ao final da string de numeros para indicar que é para realizar uma adição
                 
                 #Envio de dados para o server:
                 encoded_message = bytes(dados, "utf-8")
@@ -50,7 +50,7 @@ def client(serverName = 'localhost', serverPort = 15000):
             elif operacao == 2:
                 #Escolha dos numeros
                 numeros = input()
-                dados = numeros + ' 2'  #Adiciona 2 ao final da string de numeros para indicar que é para realizar uma subtração
+                dados = numeros + ' -'  #Adiciona 2 ao final da string de numeros para indicar que é para realizar uma subtração
                 
                 #Envio de dados para o server:
                 encoded_message = bytes(dados, "utf-8")
@@ -64,7 +64,7 @@ def client(serverName = 'localhost', serverPort = 15000):
             elif operacao == 3:
                 #Escolha dos numeros
                 numeros = input()
-                dados = numeros + ' 3'  #Adiciona 3 ao final da string de numeros para indicar que é para realizar uma multiplicação
+                dados = numeros + ' *'  #Adiciona 3 ao final da string de numeros para indicar que é para realizar uma multiplicação
                 
                 #Envio de dados para o server:
                 encoded_message = bytes(dados, "utf-8")
@@ -78,7 +78,7 @@ def client(serverName = 'localhost', serverPort = 15000):
             elif operacao == 4:
                 #Escolha dos numeros
                 numeros = input()
-                dados = numeros + ' 4'  #Adiciona 4 ao final da string de numeros para indicar que é para realizar uma divisão
+                dados = numeros + ' /'  #Adiciona 4 ao final da string de numeros para indicar que é para realizar uma divisão
                 
                 #Envio de dados para o server:
                 encoded_message = bytes(dados, "utf-8")
