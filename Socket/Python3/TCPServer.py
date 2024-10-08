ca87b3d009f815b3397b5629fbfe8419696142c1
Lara Portilho <lara.portilho.m@gmail.com>
2024-10-07 21:17:09 -0300

    feat: adicionado serrvidor, exemplo de cliente

--- Socket/Python3/TCPServer.py
+++ Socket/Python3/TCPServer.py
@@ -1,12 +1,35 @@
 from socket import *
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-print ('The server is ready to receive')
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+
+HOST = ''
+PORT = 15000
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+(conn, addr) = s.accept()
+
+try:
+  while True:
+    data = conn.recv(1024)
+    if not data: break
+
+    numeros = data.decode().split(' ')
+    operacao = numeros.pop()
+
+    if(len(numeros) > 20): 
+       conn.send('Quantidade de valores excedido!')
+       break
+
+    soma = int(numeros[0])
+    for numero in numeros[1:]:
+        if(operacao == '+'): soma+=int(numero)
+        elif(operacao == '-'): soma-=int(numero)
+        elif(operacao == '*'): soma*=int(numero)
+        elif(operacao == '/'): soma/=int(numero)
+        else:
+            conn.send('Operação inválida!')
+            break
+
+    conn.send(str(soma).encode())
+finally:
+    conn.close()
